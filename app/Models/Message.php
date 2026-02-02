bfad024523487a0495604d6871308e121de3314a
mariam <mariamemampp00@gmail.com>
2026-02-02 13:09:52 +0200

    handle the DB

---
+++ app/Models/Message.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Message extends Model
+{
+    protected $fillable = ['chat_id','sender_id','content','is_read'];
+
+    public function chat () : BelongsTo
+    {
+        return $this->belongsTo(Chat::class);
+    }
+
+    public function sender():BelongsTo
+    {
+        return $this->belongsTo(User::class, 'sender_id');
+    }
+}
